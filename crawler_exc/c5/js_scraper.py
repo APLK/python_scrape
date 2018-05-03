eec5c7168c53c36cfe3f2d509d82f1e04cc10645
likai.zqgame <1399424199@qq.com>
2018-05-03 17:49:09 +0800

    pyqt4

--- crawler_exc/c5/js_scraper.py
+++ crawler_exc/c5/js_scraper.py
@@ -13,14 +13,16 @@ import re
 import string
 from urllib.request import Request, urlopen
 
+import lxml
+
 try:
-    from PySide.QtGui import *
-    from PySide.QtCore import *
-    from PySide.QtWebKit import *
+    from PySide.QtGui import QApplication
+    from PySide.QtCore import QUrl, QEventLoop, QTimer
+    from PySide.QtWebKit import QWebView
 except ImportError:
-    from PyQt4.QtGui import *
-    from PyQt4.QtCore import *
-    from PyQt4.QtWebKit import *
+    from PyQt4.QtGui import QApplication
+    from PyQt4.QtCore import QUrl, QEventLoop, QTimer
+    from PyQt4.QtWebKit import QWebView
 
 import requests
 import time
@@ -92,4 +94,13 @@ headers = [("User-Agent", user_agent)]
 # print(tree.cssselect('#result')[0].text)
 
 app = QApplication([])
+webview = QWebView()
+loop = QEventLoop()
+webview.loadFinished.connect(loop.quit)
+webview.load(QUrl(url))
+loop.exec_()
+html = webview.page().mainFrame().toHtml()
+parser = etree.XMLParser(recover=True)
+tree = etree.fromstring(html,parser)
+print(tree.cssselect('#result')[0].text)
 
